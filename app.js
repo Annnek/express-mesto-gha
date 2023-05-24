2599b7f9b50113ad702115371a96a3fe1a7acdfc
Nekhorosheva <nekhorosheva.ann@gmail.com>
2023-05-24 14:20:27 -0700

    настроить helmet

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const helmet = require("helmet");
+
 const { errors } = require("celebrate");
 
 // const routeSignin = require("./routes/signin");
@@ -18,6 +20,7 @@ const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
